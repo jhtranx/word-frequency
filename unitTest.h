37c76ba7e84ef61345238916f92d95287aeff238
Justin Tran <52638872+jhtranx@users.noreply.github.com>
2021-08-24 18:15:39 -0700

    Update unitTest.h

--- unitTest.h
+++ unitTest.h
@@ -1,8 +1,3 @@
-/* Provided stubbed test macros for the Unit Test Macros exercise. Your job
- * is to complete them AND develop unit tests for them in the provided
- * partially complete unit test driver (testUnitTestMacros.c).
- */
-
 #ifndef UNITTEST_H
 #define UNITTEST_H
 
@@ -12,14 +7,6 @@
 #include <string.h>
 #include <math.h>
 
-/*
- * The TEST_SIGNED is given to you in the assigned reading so you should find
- * it and copy it here. Be sure to test your copy to make sure it works as
- * expected!
- *
- * AND, remember that the provided tests are not sufficient - you'll need to
- * write more!
- */
 #define TEST_SIGNED(_ACTUAL, _EXPECT)\
 {\
    long _actual = _ACTUAL, _expect = _EXPECT;\
@@ -30,9 +17,6 @@
    }\
 }
 
-/*
- * Stubbed macros you must complete.
- */
 #define TEST_UNSIGNED(_ACTUAL, _EXPECT)\
 {\
    unsigned long _actual = _ACTUAL, _expect = _EXPECT;\
